a27ed8fd8fc19268bf268fcd264fef7b60e82711
Rashed Khader <rashed.khader97@gmail.com>
2024-07-12 08:06:00 +0400

    fix

--- components/main/Whyus.tsx
+++ components/main/Whyus.tsx
@@ -8,7 +8,7 @@ const Whyus = () => {
             id="solutions"
         >
             <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
-                OUR SOLUTIONS
+                OUR SOLUTION
             </h1>
             <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10 justify-center">
                 <ProjectCard
